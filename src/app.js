8f71b77c9397b5da5c2ceaf4557c173fa2237205
ammy203 <33599273+ammy203@users.noreply.github.com>
2018-02-04 16:27:27 +0500

    Styles and responsiveness

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './components/Calculator';
 import 'normalize.css/normalize.css'
-import './styles/style.scss';
+import './styles/style.css';
 
 ReactDOM.render(<Calculator />, document.getElementById('app'));
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Operations from './Operations';
 
 class Calculator extends React.Component {
     state = {
@@ -26,21 +25,26 @@ class Calculator extends React.Component {
     };
     render() {
         return (
-            <div>
-                <h1>Simple Calculator Application In Reactjs</h1>
-                <h2>By Amir Ali</h2>.
-                <form onSubmit={this.onSubmit}>
-                    <input type='number' required id='num1' placeholder='Enter 1st Number' />
-                    <input type='number' required id='num2' placeholder='Enter 2nd Number' />
-                    <select id='operation'>
-                        <option label='Choose Operation on these numbers'></option>
-                        <option label='Add these Numbers'>+</option>
-                        <option label='Subtract these Numbers'>-</option>
-                        <option label='Multiply these Numbers'>*</option>
-                        <option label='Divide these Numbers'>/</option>
-                    </select>
-                    <button type='submit'>Calculate</button>
-                    <input type='number' placeholder='Answer will be here' value={this.state.answer} />
+            <div className='container-fluid'>
+                <div className='header'>
+                    <h1>Simple Calculator Application In Reactjs</h1>
+                    <h3>By Amir Ali</h3>
+                </div>
+                <form className='form-horizontal' onSubmit={this.onSubmit}>
+                    <div className='container-fluid'>
+                        <input type='number' required id='num1' placeholder='Enter 1st Number' />
+                        <input type='number' required id='num2' placeholder='Enter 2nd Number' />
+                        <select id='operation'>
+                            <option label='Choose Operation on these numbers'></option>
+                            <option label='Add these Numbers'>+</option>
+                            <option label='Subtract these Numbers'>-</option>
+                            <option label='Multiply these Numbers'>*</option>
+                            <option label='Divide these Numbers'>/</option>
+                        </select>
+                        <button type='submit' className='btn btn-info'>Calculate</button>
+                        <input type='number' id="answer" placeholder='Answer will be here' value={this.state.answer} />
+                    </div>
+
                 </form>
             </div>
         );
